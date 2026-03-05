285171f4d9a2767ed14b53fa343f0db8f4310f87
Rafaella <rafaella11garcia@gmail.com>
2026-03-05 20:26:25 -0300

    Adicao da classe JOoptionPane

---
+++ 05_03/src/pkg05_03/AJOptionPane.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg05_03;
+
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author Aluno
+ */
+public class AJOptionPane {
+    public static void main(String[] args) {
+        Pessoa p2 = new Pessoa();
+        
+        p2.nome = JOptionPane.showInputDialog("Qual seu nome? ");
+        
+        p2.idade = Integer.parseInt(JOptionPane.showInputDialog("Qual sua idade? "));
+        
+        p2.salario = Double.parseDouble(JOptionPane.showInputDialog("Qual sua idade? "));
+        
+        p2.imprimir();
+    }
+    
+}
