eae7fb37dc19f9b83ad3343a30f9db7d164fdaa3
Rafaella <rafaella11garcia@gmail.com>
2026-03-12 20:12:57 -0300

    Criei o objeto empresa

--- Arrays/Exe2/src/exe2/Empresa.java
+++ Arrays/Exe2/src/exe2/Empresa.java
@@ -1,19 +1,14 @@
+package exe2;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package exe2;
 
 /**
  *
  * @author Aluno
  */
-public class Funcionario {
-    String nome;
-    double salario;
-
-    @Override
-    public String toString() {
-        return "Funcionario{" + "nome=" + nome + ", salario=" + salario + '}';
-    }
- }
+public class Empresa {
+    
+}
