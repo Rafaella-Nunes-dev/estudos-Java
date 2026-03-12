514b5aaf0c2f732f3dc41feab86158f8311dcf6a
Rafaella <rafaella11garcia@gmail.com>
2026-03-12 19:43:35 -0300

    Criar vetor, pupular e printar

--- Arrays/exe1/src/exe1/Exe1.java
+++ Arrays/exe1/src/exe1/Exe1.java
@@ -5,7 +5,10 @@
 package exe1;
 
 /**
- *
+ * *
+Crie um vetor de pessoas. Este vetor deve suportar até 3 pessoas.
+Popule as 3 pessoas dentro de um lado.
+Depois que vetor tiver cheio, imprima as 3 pessoas do vetor.
  * @author Aluno
  */
 public class Exe1 {
@@ -14,7 +17,31 @@ public class Exe1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pessoa[] pessoas = new Pessoa[3];
+        
+        pessoas[0] = new Pessoa();
+        
+        pessoas[0].nome = "Rafaella";
+        pessoas[0].idade = 19;
+        pessoas[0].peso = 58.3;
+        
+        pessoas[1] = new Pessoa();
+        
+        pessoas[1].nome = "Eduardo";
+        pessoas[1].idade = 22;
+        pessoas[1].peso = 59.5;
+        
+        pessoas[2] = new Pessoa();
+        
+        pessoas[2].nome = "Marcella";
+        pessoas[2].idade = 19;
+        pessoas[2].peso = 48.3;
+        
+        System.out.println( pessoas[0].toString());
+       System.out.println( pessoas[1].toString());
+        System.out.println(pessoas[2].toString());
+        
+        
     }
     
 }
--- Arrays/exe1/src/exe1/Pessoa.java
+++ Arrays/exe1/src/exe1/Pessoa.java
@@ -5,9 +5,20 @@
 package exe1;
 
 /**
- *
+ * Crie um vetor de pessoas. Este vetor deve suportar até 3 pessoas.
+Popule as 3 pessoas dentro de um lado.
+Depois que vetor tiver cheio, imprima as 3 pessoas do vetor.
  * @author Aluno
  */
 public class Pessoa {
+    String nome;
+    int idade;
+    double peso;
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", idade=" + idade + ", peso=" + peso + '}';
+    }
+    
     
 }
